49246c48f7d4caec9402cb349d2879965f42ef8d
varfaraway <farandi.963@gmail.com>
2023-06-28 12:25:41 +0700

    ACW2-148: Sortable Tree Styling Fix

--- src/components/atoms/SortableTree/index.tsx
+++ src/components/atoms/SortableTree/index.tsx
@@ -2,6 +2,9 @@ import { SetStateAction, useState } from 'react';
 import SortableTree from '@nosferatu500/react-sortable-tree';
 import '@nosferatu500/react-sortable-tree/style.css';
 import { ISortableTree } from './types';
+import TreeTheme from "react-sortable-tree-theme-full-node-drag";
+import "./style.css";
+import DotsThreeCircle from "@/assets/dots-three-circle.svg";
 
 <style>.button {}</style>;
 
@@ -17,34 +20,27 @@ export default function SortableTreeComponent(props: ISortableTree) {
     <div className="mt-10">
       <div className="w-full py-10 h-[50vh] overflow-auto">
         <SortableTree
+          theme={TreeTheme}
           treeData={treeData}
           onChange={treeData => {
             updateTreeData(treeData);
             onChange(treeData);
           }}
-          canDrag={({ node }) => !node.dragDisabled}
+          canDrag={({ node }) => !node.noDragging}
           maxDepth={3}
           generateNodeProps={rowInfo => ({
-            // title: rowInfo.node.label,
             // subtitle: rowInfo.node.subTitle,
+            title: rowInfo.node.label,
             children: rowInfo.node.child,
             buttons: [
               <div key={1}>
                 <div className='p-2'>
-                  <svg 
-                    role='button'
-                    onClick={() => { onClick(rowInfo); }}
-                    xmlns="http://www.w3.org/2000/svg" 
-                    fill="none" 
-                    viewBox="0 0 24 24" 
-                    strokeWidth={1.5} 
-                    stroke="currentColor" 
-                    className="w-6 h-6">
-                    <path 
-                      strokeLinecap="round" 
-                      strokeLinejoin="round"
-                      d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                  </svg>
+                  <img src={DotsThreeCircle} 
+                    className='w-[24px] h-[24px]'
+                    onClick={() => {
+                      onClick(rowInfo);
+                    }}
+                  />
                 </div>
               </div>,
             ],
