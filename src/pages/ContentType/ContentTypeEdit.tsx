6f73b5e91215abcfa79c97c6dfae92d8d4ac5a27
Muhamad Firdaus <muhamad.firdaus2995@gmail.com>
2023-06-30 15:11:21 +0700

    ACW2-207: integration Update Content Type Builder Page

--- src/pages/ContentType/ContentTypeEdit.tsx
+++ src/pages/ContentType/ContentTypeEdit.tsx
@@ -70,6 +70,7 @@ export default function ContentTypeEdit() {
   useEffect(() => {
     const refetch = async () => {
       await fetchConfigQuery.refetch();
+      await fetchQuery.refetch();
     };
     void refetch();
   }, [])
