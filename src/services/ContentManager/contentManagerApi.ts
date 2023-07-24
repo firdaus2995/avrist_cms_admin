f6cf101ea975fb396ac6b63f7e5a3e819c032c4a
Muhamad Firdaus <muhamad.firdaus2995@gmail.com>
2023-07-24 11:08:39 +0700

    ACW2-444:integration archive list page in content manager

--- src/services/ContentManager/contentManagerApi.ts
+++ src/services/ContentManager/contentManagerApi.ts
@@ -37,9 +37,62 @@ export const contentManagerApi = createApi({
         variables: payload,
       }),
     }),
+    getArchiveData: builder.query<any, any>({
+      query: payload => ({
+        document: gql`
+          query contentData(
+            $id: Int!
+            $pageIndex: Int!
+            $limit: Int!
+            $sortBy: String
+            $direction: String
+            $archive: Boolean
+          ){
+            contentDataList(postTypeId: $id,
+            pageableRequest: {
+                pageIndex: $pageIndex
+                limit: $limit
+                sortBy: $sortBy
+                direction: $direction
+                isArchive: $archive
+            }) {
+                total
+                contentDataList {
+                    id
+                    title
+                    shortDesc
+                    categoryName
+                    status
+                }
+            }
+        }
+        `,
+        variables: payload,
+      }),
+    }),
+    restoreData: builder.mutation<any, any>({
+      query: payload => ({
+        document: gql`
+          mutation contentDataRestore(
+            $id: Int!
+          ){
+            contentDataRestore(id: $id) {    
+                id
+                title
+                shortDesc
+                categoryName
+                status
+            }
+        }
+        `,
+        variables: payload,
+      }),
+    }),
   })
 })
 
 export const {
   useGetContentDataQuery,
+  useGetArchiveDataQuery,
+  useRestoreDataMutation,
 } = contentManagerApi;
