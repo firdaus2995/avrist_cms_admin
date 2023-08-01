98456814bd5d52916a29477c3f241a774041da71
Deri <deriforwork@gmail.com>
2023-08-02 04:13:24 +0700

    ACW2-529: adding form validation not yet succeed

--- src/pages/ContentManager/components/FormList/TextField.tsx
+++ src/pages/ContentManager/components/FormList/TextField.tsx
@@ -1,20 +1,81 @@
 import Typography from '@/components/atoms/Typography';
-import { InputText } from '@/components/atoms/Input/InputText';
+import ErrorSmallIcon from '@/assets/error-small.svg';
 
-export default function TextField({ key, name, onChange }: any) {
+export default function TextField({
+  labelTitle,
+  type,
+  containerStyle = '',
+  value,
+  placeholder,
+  disabled,
+  onChange,
+  inputStyle,
+  direction,
+  roundStyle = 'xl',
+  themeColor,
+  inputWidth,
+  inputHeight,
+  name,
+  error,
+  suffix,
+  helperText,
+  fieldTypeLabel,
+}: any) {
   return (
-    <div key={key}>
-      <Typography type="body" size="m" weight="bold" className="w-48 mt-5 ml-1 mr-9">
-        Text Field
+    <div>
+      <Typography
+        type="body"
+        size="m"
+        weight="bold"
+        className={`w-48 ml-1 mr-9 mt-2 ${fieldTypeLabel ? 'visible' : 'hidden'}`}>
+        {fieldTypeLabel}
       </Typography>
-      <InputText
-        labelTitle={name}
-        labelStyle="font-bold text-base w-48"
-        direction="row"
-        roundStyle="xl"
-        onChange={onChange}
-      />
-      <div className="border my-10" />
+      <div className="flex flex-row">
+        <Typography type="body" size="m" weight="bold" className="w-48 mt-1 ml-1 mr-9">
+          {labelTitle}
+        </Typography>
+        <div
+          className={`
+            form-control
+            w-full
+            ${containerStyle}
+            ${direction === 'row' ? 'flex-row' : ''}
+        `}>
+          <div
+            style={{ width: inputWidth ?? '100%', height: inputHeight ?? '' }}
+            className={`
+            input
+            input-bordered 
+            rounded-${roundStyle} ${themeColor ? `border-${themeColor}` : ''} 
+            focus-within:outline 
+            focus-within:outline-2 
+            focus-within:outline-offset-2 
+            focus-within:outline-${themeColor ?? '[#D2D4D7]'} 
+            ${disabled ? 'bg-[#E9EEF4] ' : ''} 
+            ${error ? 'border-reddist' : ''}
+          `}>
+            <input
+              name={name}
+              type={type ?? 'text'}
+              value={value}
+              disabled={disabled}
+              placeholder={placeholder ?? ''}
+              onChange={onChange}
+              className={`w-full h-full rounded-3xl px-1 outline-0 ${inputStyle} ${
+                disabled ? 'text-[#637488]' : ''
+              }`}
+            />
+            <div className="relative right-8">{suffix ?? ''}</div>
+          </div>
+          {error && (
+            <div className="flex flex-row px-1 py-2">
+              <img src={ErrorSmallIcon} className="mr-3" />
+              <p className="text-reddist text-sm">{helperText}</p>
+            </div>
+          )}
+        </div>
+        <div className="border my-10" />
+      </div>
     </div>
   );
 }
