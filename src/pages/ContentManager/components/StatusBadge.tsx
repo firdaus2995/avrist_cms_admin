7fe4de82518941fa432ef1bde00deab07c0ba842
Muhamad Firdaus <muhamad.firdaus2995@gmail.com>
2023-08-14 12:14:11 +0700

    ACW2-607: bugfix satus delete approval & delete rejected

--- src/pages/ContentManager/components/StatusBadge.tsx
+++ src/pages/ContentManager/components/StatusBadge.tsx
@@ -34,9 +34,12 @@ const StatusBadge: React.FC<StatusBadgeProps> = ({ status }) => {
       title = 'Approve';
       break;
     case 'rejected':
-    case 'delete_rejected':
       style = 'bg-[#EBD2CE] border-[#D09191]';
       title = 'Rejected';
+      break;
+    case 'delete_rejected':
+      style = 'bg-[#EBD2CE] border-[#D09191]';
+      title = 'Delete Rejected';
       break;
     default:
       style = 'bg-[#E4E4E4] border-[#A9AAB5]';
