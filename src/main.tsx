99d7e9c6f1dace02c0775e5c8f76fc7adee670fe
varfaraway <farandi.963@gmail.com>
2024-01-22 09:31:45 +0700

    Change BASE URL

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/admin'>
       <Provider store={store}>
         <App />
       </Provider>
