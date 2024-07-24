641cf9c8d7cf10afd9ee7a1a6de16cc92361d5f2
farizerz <fariz.rizki.ramadhan@gmail.com>
2024-07-24 15:51:43 +0700

    ACW2-00: Change Content Detail Color When Not Edited

--- src/components/atoms/Ckeditor/index.tsx
+++ src/components/atoms/Ckeditor/index.tsx
@@ -3,6 +3,7 @@ import { t } from 'i18next';
 
 import ErrorSmallIcon from '@/assets/error-small.svg';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
+import './App.css';
 
 interface ICkEditor {
   data?: string;
@@ -20,26 +21,27 @@ export default function CkEditor({
   helperText,
 }: ICkEditor) {
   return (
-    <div>
-      <CKEditor
-        disabled={disabled}
-        data={data ?? ''}
-        editor={Editor}
-        onChange={(_event: any, editor: any) => {
-          const data = editor.getData();
-          if (onChange) {
-            onChange(data);
-          };
-        }}
-      />
-      {
-        isError && (
-          <div className='flex flex-row px-1 py-2'>
-            <img src={ErrorSmallIcon} className='mr-3' />
-            <p className='text-reddist text-sm'>{helperText ?? t('components.atoms.required')}</p>
-          </div>
-        )
-      }    
+    <div className="editor-container mb-4">
+      <div className="editor-wrapper">
+        <CKEditor
+          disabled={disabled}
+          data={data ?? ''}
+          editor={Editor}
+          onChange={(_event: any, editor: any) => {
+            const data = editor.getData();
+            if (onChange) {
+              onChange(data);
+            }
+          }}
+        />
+        {disabled && <div className="editor-overlay"></div>}
+      </div>
+      {isError && (
+        <div className="flex flex-row px-1 py-2">
+          <img src={ErrorSmallIcon} className="mr-3" alt="Error icon" />
+          <p className="text-reddist text-sm">{helperText ?? t('components.atoms.required')}</p>
+        </div>
+      )}
     </div>
   );
 }
