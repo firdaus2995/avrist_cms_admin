4abf63a6f3c1dd380de642b2cd612d98b791135b
farizerz <fariz.rizki.ramadhan@gmail.com>
2024-07-30 21:07:42 +0700

    ACW2-00: Fix Approve & Reject Button Position on Page Management Detail

--- src/components/molecules/ButtonMenu/index.tsx
+++ src/components/molecules/ButtonMenu/index.tsx
@@ -8,25 +8,25 @@ interface IButtonMenu {
   onClickApprove?: any;
   onClickReject?: any;
 }
-export const ButtonMenu: React.FC<IButtonMenu> = ({onClickApprove, onClickReject}) => {
+export const ButtonMenu: React.FC<IButtonMenu> = ({ onClickApprove, onClickReject }) => {
   return (
     <div className="dropdown dropdown-end">
       <label tabIndex={0} className="btn btn-outline btn-primary m-1 w-44">
-      {t('components.molecules.response')}
+        {t('components.molecules.response')}
       </label>
       <ul
         tabIndex={0}
         className="dropdown-content border-[1px] menu p-2 shadow bg-base-100 rounded-box w-52">
-        <li role='button' onClick={onClickApprove}>
-          <label tabIndex={0} className="btn btn-outline btn-primary m-1 w-44">
+        <li role="button" onClick={onClickApprove}>
+          <label tabIndex={0} className="btn btn-outline btn-primary my-1 -ml-6 w-44">
             <img src={CheckCircle} />
-          {t('components.molecules.approve')}
+            {t('components.molecules.approve')}
           </label>
         </li>
-        <li role='button' onClick={onClickReject}>
-          <label tabIndex={0} className="btn btn-outline btn-primary m-1 w-44">
+        <li role="button" onClick={onClickReject}>
+          <label tabIndex={0} className="btn btn-outline btn-primary my-1 -ml-6 w-44">
             <img src={RejectUser} />
-          {t('components.molecules.reject')}
+            {t('components.molecules.reject')}
           </label>
         </li>
       </ul>
