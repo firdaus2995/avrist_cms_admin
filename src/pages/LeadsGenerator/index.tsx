c0769e5da96ce2bd33334501ec4e8606b550aff0
farizerz <fariz.rizki.ramadhan@gmail.com>
2024-10-07 15:06:45 +0700

    ACW2-5231: Fix Empty Page when there is no question on Leads Generator

--- src/pages/LeadsGenerator/index.tsx
+++ src/pages/LeadsGenerator/index.tsx
@@ -51,15 +51,15 @@ const initialData = [
     isDelete: false,
     answers: [
       {
-        id: null,
-        answerOrder: '',
+        id: 1,
+        answerOrder: 'A',
         answerDesc: '',
         weight: 0,
         action: 'create',
       },
       {
-        id: null,
-        answerOrder: '',
+        id: 2,
+        answerOrder: 'B',
         answerDesc: '',
         weight: 0,
         action: 'create',
@@ -159,7 +159,7 @@ const LeadsGenerator = () => {
           answers: question.answers.map((answer: IAnswer) => ({ ...answer, action: 'edit' })),
         };
       });
-      type === 'fetch' && setQuestion(filteredList);
+      type === 'fetch' && setQuestion(filteredList.length > 0 ? filteredList : initialData);
       defaultQuestion === initialData && setDefaultQuestion(filteredList);
 
       for (let i = 0; i < list.length; i++) {
@@ -385,7 +385,7 @@ const LeadsGenerator = () => {
                                       }
                                       return i.id === jtem.id
                                         ? { ...i, action: 'delete' }
-                                        : idx > 1
+                                        : idx > 1 && i.answerOrder === 'D'
                                         ? {
                                             ...i,
                                             answerOrder: String.fromCharCode(
@@ -429,6 +429,8 @@ const LeadsGenerator = () => {
                                 temp[i].answers.filter((item: any) => item.action !== 'delete')
                                   .length - 1
                               ];
+                            newOption.id =
+                              Number(temp[i].answers[temp[i].answers.length - 1]?.id ?? 0) + 1;
                             const previousOrder = previousData?.answerOrder;
                             const newOrder = String.fromCharCode(
                               (previousOrder as string).charCodeAt(0) + 1,
